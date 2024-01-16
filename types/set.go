787825b1522683b455f8771db9d2ba56faff90cd
ramboll.wong <ramboll.wong@hotmail.com>
2024-01-16 17:49:54 +0800

    add range function for set. add comments for set.

--- types/set.go
+++ types/set.go
@@ -5,15 +5,19 @@ import (
 	"sync/atomic"
 )
 
+// Set represents a thread-safe set data structure that stores unique elements of type T.
 type Set[T any] struct {
 	size int64
 	m    sync.Map
 }
 
+// NewSet creates a new instance of the Set data structure.
 func NewSet[T any]() *Set[T] {
 	return &Set[T]{}
 }
 
+// Put adds an element to the set.
+// It returns a boolean indicating whether the element was added successfully (true if added, false if already exists).
 func (s *Set[T]) Put(v T) bool {
 	_, ok := s.m.LoadOrStore(v, struct{}{})
 	if !ok {
@@ -22,6 +26,8 @@ func (s *Set[T]) Put(v T) bool {
 	return !ok
 }
 
+// Pop removes an element from the set.
+// It returns the removed element and a boolean indicating whether the element existed in the set.
 func (s *Set[T]) Pop(v T) (T, bool) {
 	t, ok := s.m.LoadAndDelete(v)
 	if ok {
@@ -30,16 +36,29 @@ func (s *Set[T]) Pop(v T) (T, bool) {
 	return t.(T), ok
 }
 
+// Remove removes an element from the set.
+// It returns a boolean indicating whether the element was successfully removed (true if removed, false if not found).
 func (s *Set[T]) Remove(v T) bool {
 	_, ok := s.Pop(v)
 	return ok
 }
 
+// Exist checks if an element exists in the set.
+// It returns a boolean indicating whether the element exists in the set (true if exists, false if not found).
 func (s *Set[T]) Exist(v T) bool {
 	_, ok := s.m.Load(v)
 	return ok
 }
 
+// Size returns the current size of the set.
 func (s *Set[T]) Size() int64 {
 	return atomic.LoadInt64(&s.size)
+}
+
+// Range iterates over all elements in the set and calls the provided function for each element.
+// It stops iteration if the function returns false.
+func (s *Set[T]) Range(f func(t T) bool) {
+	s.m.Range(func(key, _ any) bool {
+		return f(key.(T))
+	})
 }
